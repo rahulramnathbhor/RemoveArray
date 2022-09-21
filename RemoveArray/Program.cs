cdfeab7d05fec95f5520edd7c5acbb154824f47c
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-21 20:08:53 +0530

    Add project files.

---
+++ RemoveArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveArray
+{
+    internal class Program
+    {
+      
+        public class Example
+        {
+            public static void Main()
+            {
+                int[] array = { 1, 3, 4, 5, 4, 2,1 };
+                int item = 2 ;
+
+                List<int> nums = new List<int>(array);
+                nums.RemoveAt(nums.IndexOf(item));
+                array = nums.ToArray();
+
+                Console.WriteLine(String.Join(",", array));
+            }
+        
+        }
+    }
+}
